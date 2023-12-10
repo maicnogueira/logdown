6b0b6e1770d060c25953706de1b77a7c2a725de3
Maic Nogueira <joao-maic@petiko.com.br>
2023-12-09 22:31:54 -0300

    logdown twste

--- src/LogDownProvider.php
+++ src/LogDownProvider.php
@@ -3,6 +3,7 @@
 namespace LogDown;
 
 use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 
@@ -10,14 +11,19 @@ class LogDownProvider extends ServiceProvider implements DeferrableProvider
 {
     public function boot()
     {
-
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        parent::boot();
+        Route::middleware('web')
+            ->namespace('LogDown')
+            ->group(function () {
+                $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+            });
+//        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
 
     }
 
     public function register()
     {
-
+        $this->app->register(LogDownProvider::class);
 
     }
 
--- src/routes/web.php
+++ src/routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use LogDown\Controllers\TesteController;
 
-Route::group(['namespace' => 'LogDown\Controllers', 'middleware' => ['web']], function () {
+Route::group(['middleware' => ['web']], function () {
     Route::get('/minha-rota', TesteController::class.'@index');
 });
