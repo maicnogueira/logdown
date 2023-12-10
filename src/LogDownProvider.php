8609c0ec6ff2540d600f5f60011d263e19c59a8a
Maic Nogueira <joao-maic@petiko.com.br>
2023-12-10 03:50:36 -0300

    logdown twste

--- src/LogDownProvider.php
+++ src/LogDownProvider.php
@@ -12,13 +12,8 @@ class LogDownProvider extends ServiceProvider implements DeferrableProvider
 {
     public function boot()
     {
-        parent::boot();
-        Route::middleware('web')
-            ->namespace(TesteController::class)
-            ->group(function () {
-                $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-            });
-//        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
 
     }
 
